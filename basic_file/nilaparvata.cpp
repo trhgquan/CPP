ee14fab906f4422f44decb2f66649e4849ef0d27
trhgquan <mrthedzam@gmail.com>
2018-10-20 23:22:33 +0700

    added nilaparvata.cpp

---
+++ basic_file/nilaparvata.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int W, H, square, square_max = 0;
+int a[10000][10000], b[10000][10000];
+
+void check (int i, int j)
+{
+    a[i][j] = 0;
+    b[square][0] = i + 1;
+    b[square][1] = j + 1;
+    ++square;
+    if (i < H && a[i + 1][j] == 1)
+        check(i + 1, j);
+    if (j < W && a[i][j + 1] == 1)
+        check(i, j + 1);
+    if (i > 0 && a[i - 1][j] == 1)
+        check(i - 1, j);
+    if (j > 0 && a[i][j - 1] == 1)
+        check(i, j - 1);
+}
+
+int main() {
+    std::ifstream afile("nilaparvata.inp", std::ios::in);
+    std::ofstream bfile("nilaparvata.out", std::ios::out);
+
+    afile >> H >> W;
+
+    for (int i = 0; i < H; i++)
+        for (int j = 0; j < W; j++)
+            afile >> a[i][j];
+
+    for (int i = 0; i < H; i++) {
+        square = 0;
+        for (int j = 0; j < W; j++) {
+            if (a[i][j] == 1)
+                check(i,j);
+        }
+        if (square != 0)
+        {
+            bfile << square;
+            for (int k = 0; k < square; k++) {
+                bfile << '\t' << "[" << b[k][0] << ", " << b[k][1] << "]";
+            }
+            bfile << '\n';
+        }
+    }
+
+    afile.close();
+    bfile.close();
+    return 0;
+}

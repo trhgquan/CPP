7d92f4f304a64b8198419236777015b0e5145d80
trhgquan <mrthedzam@gmail.com>
2018-12-02 00:38:36 +0700

    so sleepy, but i learnt FLEURY

---
+++ basic_file/EULER/FLEURY.cpp
@@ -0,0 +1,67 @@
+// FLEURY algorithm - Euler graph
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <string.h>
+
+#define NMAX 101
+
+using namespace std;
+
+ifstream afile("FLEURY.inp", ios::in);
+ofstream bfile("FLEURY.out", ios::out);
+
+int graph[NMAX][NMAX], VER[NMAX];
+int n, count = 0;
+
+stack<int> stk;
+
+void read();
+void print();
+
+int main() {
+    read();
+
+    stk.push(1);
+
+    while (!stk.empty()) {
+        int u = stk.top();
+        for (int v = 1; v <= n; v++) {
+            if (graph[u][v] > 0) {
+                graph[u][v]--;
+                graph[v][u]--;
+                stk.push(v);
+                break;
+            }
+        }
+        if (stk.top() == u) {
+            VER[count] = stk.top();
+            count++;
+            stk.pop();
+        }
+    }
+
+    print();
+
+    afile.close();
+    bfile.close();
+    return 0;
+}
+
+void read() {
+    int u, v, k;
+
+    memset(graph, 0, sizeof(graph));
+
+    afile >> n;
+    while (afile >> u >> v >> k) {
+        graph[u][v] = k;
+        graph[v][u] = k;
+    }
+}
+
+void print() {
+    for (int i = count - 1; i >= 0; i--) {
+        bfile << VER[i] << '\t';
+    }
+}

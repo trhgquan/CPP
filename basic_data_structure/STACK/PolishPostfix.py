6535eb38f62fea7f04c74d3b59db82be2a7ccd9a
trhgquan <quanthcstpbt@gmail.com>
2019-09-22 11:41:50 +0700

    Polish Notation Postfix bug fixed.

--- basic_data_structure/STACK/PolishPostfix.py
+++ basic_data_structure/STACK/PolishPostfix.py
@@ -1,10 +1,10 @@
 # THIS CALCULATE Polish Postfix Notation
 
 # Uncomment the code below if you use deque
-#from collections import deque
+from collections import deque
 
 # Uncomment the code below if you use LifoQueue
-from queue import LifoQueue
+#from queue import LifoQueue
 
 # Declare the math signs
 mathSigns = ["+", "-", "*", "/"]
@@ -21,57 +21,66 @@ def calculate(number_1, number_2, sign):
     else: return number_1 / number_2
 
 def PolishNotation_postfix(notation):
-    # Define a Polish Stack. This does NOT require any library
+    # Define a Polish Stack list. This does NOT require any library
+
     #polishStack = []
 
     # Or, using deque (collections)
-    #polishStack = deque()
+    polishStack = deque()
 
     # Or, using LifoQueue
-    polishStack = LifoQueue()
+    #polishStack = LifoQueue()
+
+    # This variable store a number with many characters.
+    # When reading a string, '10' can be read into '1' and '0',
+    # so this prevent it happens.
+
+    numTemp = ''
 
     for character in notation:
-        # Remove spaces from string
-        if character == ' ': continue
+        # Meet a space aka a number is read.
+        if character == ' ':
+            if len(numTemp) > 0:
+                # Uncomment the code below if you use list or deque
+                polishStack.append(int(numTemp))
 
-        if not character in mathSigns:
-            # This means "character" is a number
-            
-            # Uncomment the code below if you use list or deque
-            #polishStack.append(int(character))
+                # Uncomment the code below if you use LifoQueue
+                #polishStack.put_nowait(int(numTemp))
 
-            # Uncomment the code below if you use LifoQueue
-            polishStack.put_nowait(int(character))
-        else: 
-            # This means "character" is a math sign
-            # Note: in Polish Postfix, [item1, item2] means item2 *doing something* item1
+            numTemp = ''
+        else:
+            if not character in mathSigns:
+                # This means "character" is a number.
+                numTemp += str(character)
+            else:
+                # This means "character" is a math sign
+                # Note: in Polish Postfix, [item1, item2] means item2 *doing something* item1
 
-            # Uncomment the code below if you use list or deque
-            #number_2 = polishStack.pop()
-            #number_1 = polishStack.pop()
-            #polishStack.append(calculate(number_1, number_2, character))
+                # Uncomment the code below if you use list or deque
+                number_2 = polishStack.pop()
+                number_1 = polishStack.pop()
+                polishStack.append(calculate(number_1, number_2, character))
 
-            # Uncomment the code below if you use LifoQueue
-            number_2 = polishStack.get_nowait()
-            number_1 = polishStack.get_nowait()
-            polishStack.put_nowait(calculate(number_1, number_2, character))
+                # Uncomment the code below if you use LifoQueue
+                #number_2 = polishStack.get_nowait()
+                #number_1 = polishStack.get_nowait()
+                #polishStack.put_nowait(calculate(number_1, number_2, character))
 
     # The Stack's top item is the result.
 
     # Uncomment the code below if you use list or deque
-    #return polishStack.pop()
+    return polishStack.pop()
 
     # Uncomment the code below if you use LifoQueue
-    return polishStack.get_nowait()
+    #return polishStack.get_nowait()
 
 testCases = [
     '1 1 +', # Result: 2
-    '2 1 -', # Result: 1
-    '1 1 1 + +', # Result: 3
-    '4 5 * 1 + 2 -', # Result: 19
+    '2 3 + 4 *', # Result: 20
+    '10 5 + 3 *', # Result: 45
 
-    # Add more testcases here
+    # add more testcases here
 ]
 
 for notations in testCases:
-    print("Polish Notation Postfix \"{}\"\nresult: {}".format(notations, PolishNotation_postfix(notations)))
+    print("polish notation postfix \"{}\"\nresult: {}".format(notations, PolishNotation_postfix(notations)))

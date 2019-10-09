cef5c194b741e18d5087d6d5ea69756852a3b2ae
trhgquan <quanthcstpbt@gmail.com>
2019-10-09 17:07:40 +0700

    added QBHV.cpp

---
+++ sth_else/SPOJ/QBHV.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    string s;
+    string r;
+    int c = 0;
+
+    getline(cin, s);
+    // next_permutation requires sorted array
+    sort(s.begin(), s.end());
+
+    do {
+        r += s + '\n'; c++;
+    } while (next_permutation(s.begin(), s.end()));
+    cout << c << endl;
+    cout << r;
+
+    return 0;
+}

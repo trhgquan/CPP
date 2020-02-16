0bb6f900bcd3cd1a801c3e6e6d657e84b18c49df
trhgquan <quanthcstpbt@gmail.com>
2020-02-16 10:52:08 +0700

    update PTITSU1D

--- sth_else/SPOJ/PTITSU1D.c
+++ sth_else/SPOJ/PTITSU1D.c
@@ -6,27 +6,26 @@
 #include<stdio.h>
 
 int main() {
-    // freopen("input.txt", "r", stdin);
-	int T; scanf("%d", &T);
-	while (T--) {
-	    char G[501][501];
-	    int M, N; scanf("%d%d", &M, &N);
-	    for (int i = 0; i < 5 * M + 1; ++i)
-	        scanf("%s", G[i]);
+    int T; scanf("%d", &T);
+    while (T--) {
+        char G[501][501];
+        int M, N; scanf("%d%d", &M, &N);
+        for (int i = 0; i < 5 * M + 1; ++i)
+        scanf("%s", G[i]);
 
-	    int count[5] = {0};
-	    for (int i = 1; i < 5 * M + 1; i += 5) {
-	        for (int j = 1; j < 5 * N + 1; j += 5){
-	            if (G[i + 3][j] == '*') ++count[4];
-	            else if (G[i + 2][j] == '*') ++count[3];
-	            else if (G[i + 1][j] == '*') ++count[2];
-	            else if (G[i][j] == '*') ++count[1];
-	            else ++count[0];
-	        }
-	    }
+        int count[5] = {0};
+        for (int i = 1; i < 5 * M + 1; i += 5) {
+            for (int j = 1; j < 5 * N + 1; j += 5){
+            if (G[i + 3][j] == '*') ++count[4];
+            else if (G[i + 2][j] == '*') ++count[3];
+            else if (G[i + 1][j] == '*') ++count[2];
+            else if (G[i][j] == '*') ++count[1];
+            else ++count[0];
+            }
+        }
 
-	    for (int i = 0; i < 5; ++i) printf("%d ", count[i]);
-	    printf("\n");
-	}
+        for (int i = 0; i < 5; ++i) printf("%d ", count[i]);
+        printf("\n");
+    }
     return 0;
 }

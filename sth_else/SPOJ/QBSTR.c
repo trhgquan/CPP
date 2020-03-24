e141af00bf7f71133380b8ae1559e8fa942b6f74
trhgquan <quanthcstpbt@gmail.com>
2020-03-24 23:54:39 +0700

    update QBSTR

--- sth_else/SPOJ/QBSTR.c
+++ sth_else/SPOJ/QBSTR.c
@@ -2,6 +2,7 @@
  * QBSTR - https://vn.spoj.com/problems/QBSTR
  * Code by @trhgquan - https://github.com/trhgquan
  */
+
 #include<stdio.h>
 #include<string.h>
 
@@ -13,18 +14,16 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-void Initialize() {
+int main() {
+    scanf("%s", xauA); scanf("%s", xauB);
+
+    // Initialise
     for (unsigned i = 0; i <= strlen(xauA); ++i)
         F[i][0] = 0;
     for (unsigned i = 0; i <= strlen(xauB); ++i)
         F[0][i] = 0;
-}
 
-int main() {
-    gets(xauA);
-    gets(xauB);
-    Initialize();
-
+    // Dynamic counting
     for (unsigned i = 1; i <= strlen(xauA); ++i) {
         for (unsigned j = 1; j <= strlen(xauB); ++j) {
             if (xauA[i - 1] == xauB[j - 1])
@@ -33,6 +32,7 @@ int main() {
         }
     }
 
+    // Print the result
     printf("%d", F[strlen(xauA)][strlen(xauB)]);
     return 0;
 }

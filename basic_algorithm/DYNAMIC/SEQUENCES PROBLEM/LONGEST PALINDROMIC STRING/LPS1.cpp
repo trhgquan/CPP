c8a8c9d0bdc0c37db4231f1f364b3e32831987c5
trhgquan <quanthcstpbt@gmail.com>
2020-03-28 00:21:24 +0700

    update DP method for LPS1

--- basic_algorithm/DYNAMIC/SEQUENCES PROBLEM/LONGEST PALINDROMIC STRING/LPS1.cpp
+++ basic_algorithm/DYNAMIC/SEQUENCES PROBLEM/LONGEST PALINDROMIC STRING/LPS1.cpp
@@ -9,6 +9,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// This is the naive recursive approach
 int LPS(string sequence, int i, int j) {
     // If there is only 1 character
     if (i == j) return 1;
@@ -29,7 +30,28 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
     string s; cin >> s;
-    cout << "Length of the longest palindromic subsequence: " << LPS(s, 0, s.size() - 1);
 
+    // Using naive recursive approach
+    cout << "Length of the longest palindromic subsequence, using naive recursive method: " << LPS(s, 0, s.size() - 1) << endl;
+
+    // Using dynamic programming approach
+
+    // Build a table
+    vector<vector<int>>table(s.size(), vector<int>(s.size()));
+    // String length 1 is a palindrome itself
+    for (unsigned i = 0; i < s.size(); ++i) table[i][i] = 1;
+    int res = 0; // result
+    for (unsigned k = 2; k <= s.size(); ++k) {
+        for (unsigned i = 0; i < s.size() - k + 1; ++i) {
+            unsigned j = i + k - 1;
+
+            if (s[i] == s[j] && k == 2) table[i][j] = 2;
+            else if (s[i] == s[j]) table[i][j] = table[i + 1][j - 1] + 2;
+            else table[i][j] = max(table[i + 1][j], table[i][j - 1]);
+
+            if (table[i][j] > res) res = table[i][j];
+        }
+    }
+    cout << "Length of the longest palindromic subsequence, using DP method: " << res;
     return 0;
 }

a93e4f70a08c2e5ab79081e35688bdd6145fb754
trhgquan <quanthcstpbt@gmail.com>
2020-05-05 17:04:13 +0700

    small pointer_and_constants edit

--- sth_else/CS102/pointer_and_memory_allocation/pointer_and_constants.c
+++ sth_else/CS102/pointer_and_memory_allocation/pointer_and_constants.c
@@ -1,25 +1,32 @@
+/**
+ * This is a small example for * const (pointer to constant) and const * (constant pointer).
+ *
+ * CS102 practice only.
+ */
+
 #include<stdio.h>
 
 int main() {
     int x = 5;
-    int* const p = &x;
-    const int* q = &x;
+    int* const p = &x; // This called "constant pointer".
+    const int* q = &x; // This called "pointer to a constant".
 
     printf("x has address %p, has value %d\n", &x, x);
     printf("p is storing %p, has value %d\n", p, *p);
     printf("q is storing %p, has value %d\n", q, *q);
 
     int y = 6;
-    q = &y; // can't do: *q = y
     *p = y; // can't do: p = &y
+    q = &y; // can't do: *q = y
 
     printf("x has address %p, has value %d\n", &x, x);
     printf("p is storing %p, has value %d\n", p, *p);
     printf("q is storing %p, has value %d\n", q, *q);
 
     /**
-     * Overall, you can't change what a const* pointer point to,
-     * and can't change the value of what *const point to.
+     * Overall:
+     * you can't change what a *const pointer point to,
+     *     can't change the value of what const* point to.
      */
 
 

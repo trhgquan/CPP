16a8d2c9b0e8c25dcbfe285ca219db346884ade6
trhgquan <quanthcstpbt@gmail.com>
2020-05-08 15:26:47 +0700

    matrix_multiplication_naive now fits the REAL procedure.

--- basic_math/linear-algebra/matrix/matrix_multiplication_naive.c
+++ basic_math/linear-algebra/matrix/matrix_multiplication_naive.c
@@ -4,15 +4,33 @@
  * This is just a naive approach, for faster computing
  * on small-integer-matrices.
  *
- * Let A = nxm matrix,
- *     B = mxn matrix,
- * Result matrix, called C, will have the size nxn.
+ * Let A = R1xC1 matrix,
+ *     B = R2xC2 matrix,
+ * Result matrix, called D, will have the size R1xC2.
+ *
+ * INPUT:
+ * First round will be four integers R1, C1, R2 and C2, space separated.
+ * Next R1 rows, each row has C1 numbers, space separated.
+ * Next R2 rows, each row has C2 integers, space separated.
  *
  * Code by @trhgquan - https://github.com/trhgquan
  */
 
 #include<stdio.h>
 #include<stdlib.h>
+
+/**
+ * Read a RxC matrix.
+ * @param m pointer to a matrix.
+ * @param R rows of the matrix.
+ * @param C columns of the matrix.
+ */
+void readMatrix(int** m, int R, int C) {
+    for (int i = 0; i < R; ++i) {
+        m[i] = (int*)malloc(sizeof(R) * C);
+        for (int j = 0; j < C; ++j) scanf("%d", &m[i][j]);
+    }
+}
 
 /**
  * This function print a matrix.
@@ -22,58 +40,77 @@
  */
 void printMatrix(int** m, int R, int C) {
     for (int i = 0; i < R; ++i) {
-        for (int j = 0; j < C; ++j) printf("%d ", m[i][j]);
+        for (int j = 0; j < C; ++j) {
+            // Small output formatting:
+            // Add a space before a positive integer to fit the column,
+            // if that column has a negative.
+            if (m[i][j] >= 0) printf(" ");
+            printf("%d ", m[i][j]);
+        }
         printf("\n");
     }
+}
+
+/**
+ * Free a matrix,
+ * (built this because there are 3 matrices
+ * and I'm not gonna write 3 loops).
+ * @param m pointer to the matrix
+ * @param R the matrix's rows.
+ */
+void releaseMatrix(int** m, int R) {
+    for (int i = 0; i < R; ++i)
+        free(m[i]);
+    free(m);
 }
 
 int main() {
     freopen("matrix_multiplication.inp", "r", stdin);
 
     // Get matrices row and column.
-    int R, C; scanf("%d%d", &R, &C);
+    int R1, C1, R2, C2; scanf("%d%d%d%d", &R1, &C1, &R2, &C2);
+
+    // if C1 != R2 then we can't calculate,
+    // and stop the program here.
+    if (C1 != R2) {
+        printf("Can't calculate with these inputs");
+        return 0;
+    }
 
     // Initalise two 2-d matrix, using dynamic memory allocation.
-    int** first_matrix  = (int**)malloc(R);
-    int** second_matrix = (int**)malloc(C);
-    int** result_matrix = (int**)malloc(R);
+    int** result_matrix = (int**)malloc(R1);
+    int** first_matrix  = (int**)malloc(R1);
+    int** second_matrix = (int**)malloc(R2);
 
     // Get the first matrix, size RxC
-    for (int i = 0; i < R; ++i) {
-        first_matrix[i] = (int*)malloc(sizeof(R) * C);
-        for (int j = 0; j < C; ++j) scanf("%d", &first_matrix[i][j]);
-    }
+    readMatrix(first_matrix, R1, C1);
 
     // Get the second Matrix, size CxR
-    for (int i = 0; i < C; ++i) {
-        second_matrix[i] = (int*)malloc(sizeof(C) * R);
-        for (int j = 0; j < R; ++j) scanf("%d", &second_matrix[i][j]);
-    }
+    readMatrix(second_matrix, R2, C2);
 
     // Multiplies two matrices.
-    for (int i = 0; i < R; ++i) {
-        result_matrix[i] = (int*)malloc(sizeof(R) * R);
-        for (int j = 0; j < R; ++j) {
+    // Since writing a function will requires too many parameters,
+    // I'll not do that.
+    for (int i = 0; i < R1; ++i) {
+        result_matrix[i] = (int*)malloc(sizeof(R1) * C2);
+        for (int j = 0; j < C2; ++j) {
             result_matrix[i][j] = 0;
-            for (int k = 0; k < C; ++k)
+            for (int k = 0; k < R2; ++k)
                 result_matrix[i][j] += first_matrix[i][k] * second_matrix[k][j];
         }
     }
 
     // Print the result matrix.
-    printMatrix(result_matrix, R, R);
+    printMatrix(result_matrix, R1, C2);
 
-    // Release the first Matrix
-    for (int i = 0; i < R; ++i) free(first_matrix[i]);
-    free(first_matrix);
+    // Free first matrix
+    releaseMatrix(first_matrix, R1);
 
-    // Release the second Matrix
-    for (int i = 0; i < C; ++i) free(second_matrix[i]);
-    free(second_matrix);
+    // Free second matrix
+    releaseMatrix(second_matrix, R2);
 
-    // Release the result matrix
-    for (int i = 0; i < R; ++i) free(result_matrix[i]);
-    free(result_matrix);
+    // Free result matrix
+    releaseMatrix(result_matrix, R1);
 
     return 0;
 }

8fd737e277a622f5e6a0123c750bd5f5709884a5
trhgquan <quanthcstpbt@gmail.com>
2020-08-08 21:46:16 +0700

    added 1393A

---
+++ sth_else/MISC/1393A.cpp
@@ -0,0 +1,20 @@
+/**
+ * CODE - LINK
+ *
+ * Code by @trhgquan - https://github.com/trhgquan
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+  int T; cin >> T;
+  while (T--) {
+    int n; cin >> n;
+    cout << n / 2 + 1 << endl;
+  }
+
+  return 0;
+}
